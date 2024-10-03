be346be27285bc91b6fbb286d651501f304de83e
mohammed elhaj sami <mohamedelhaj2000@gmail.com>
2024-10-04 00:37:31 +0200

    4th method added in database manger

--- SQLiteViewer/DatabaseManager2.cs
+++ SQLiteViewer/DatabaseManager2.cs
@@ -166,10 +166,8 @@ namespace SQLiteViewer
                 connection.Open();
                 var command = connection.CreateCommand();
 
-                // Base query
                 command.CommandText = "SELECT * FROM BetterKillfeed WHERE 1 = 1";
 
-                // Add filters dynamically
                 if (!string.IsNullOrEmpty(fileName))
                 {
                     command.CommandText += " AND FileName LIKE @fileName";
@@ -188,13 +186,11 @@ namespace SQLiteViewer
                     command.Parameters.AddWithValue("@actionee", $"%{actionee}%");
                 }
 
-                // Sorting
                 if (!string.IsNullOrEmpty(orderBy))
                 {
                     command.CommandText += $" ORDER BY {orderBy} {(ascending ? "ASC" : "DESC")}";
                 }
 
-                // Pagination
                 command.CommandText += " LIMIT @pageSize OFFSET @offset";
                 command.Parameters.AddWithValue("@pageSize", pageSize);
                 command.Parameters.AddWithValue("@offset", (pageNumber - 1) * pageSize);
@@ -220,6 +216,79 @@ namespace SQLiteViewer
                             ReplayDate = reader.GetDateTime(reader.GetOrdinal("ReplayDate")),
                             ActionerId = reader.GetString(reader.GetOrdinal("ActionerId")),
                             ActioneeId = reader.GetString(reader.GetOrdinal("ActioneeId"))
+                        });
+                    }
+                }
+            }
+
+            return replays;
+        }
+
+        // Method for A_RosterWithCount with dynamic filters, pagination, and sorting
+        public ObservableCollection<A_RosterWithCount> FilterAndPaginateRosterWithCount(
+            string fileName, DateTime? replayDate, string displayName, string orderBy, bool ascending, int pageNumber, int pageSize)
+        {
+            var replays = new ObservableCollection<A_RosterWithCount>();
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+
+                command.CommandText = "SELECT * FROM A_RosterWithCount WHERE 1 = 1";
+
+                if (!string.IsNullOrEmpty(fileName))
+                {
+                    command.CommandText += " AND FileName LIKE @fileName";
+                    command.Parameters.AddWithValue("@fileName", $"%{fileName}%");
+                }
+
+                if (replayDate.HasValue)
+                {
+                    command.CommandText += " AND Date = @replayDate";
+                    command.Parameters.AddWithValue("@replayDate", replayDate.Value.ToString("yyyy-MM-dd"));
+                }
+
+                if (!string.IsNullOrEmpty(displayName))
+                {
+                    command.CommandText += " AND DisplayName LIKE @displayName";
+                    command.Parameters.AddWithValue("@displayName", $"%{displayName}%");
+                }
+
+                if (!string.IsNullOrEmpty(orderBy))
+                {
+                    command.CommandText += $" ORDER BY {orderBy} {(ascending ? "ASC" : "DESC")}";
+                }
+
+                command.CommandText += " LIMIT @pageSize OFFSET @offset";
+                command.Parameters.AddWithValue("@pageSize", pageSize);
+                command.Parameters.AddWithValue("@offset", (pageNumber - 1) * pageSize);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        replays.Add(new A_RosterWithCount
+                        {
+                            Num = reader.GetInt32(reader.GetOrdinal("Num")),
+                            Date = reader.GetDateTime(reader.GetOrdinal("Date")),
+                            Playlist = reader.GetString(reader.GetOrdinal("Playlist")),
+                            PlayerId = reader.GetString(reader.GetOrdinal("PlayerId")),
+                            DisplayName = reader.GetString(reader.GetOrdinal("DisplayName")),
+                            Lvl = reader.GetInt32(reader.GetOrdinal("Lvl")),
+                            Place = reader.GetInt32(reader.GetOrdinal("Place")),
+                            Anon = reader.GetInt32(reader.GetOrdinal("Anon")),
+                            Platform = reader.GetString(reader.GetOrdinal("Platform")),
+                            Team = reader.GetInt32(reader.GetOrdinal("Team")),
+                            Kills = reader.GetInt32(reader.GetOrdinal("Kills")),
+                            BotKills = reader.GetInt32(reader.GetOrdinal("BotKills")),
+                            Crowns = reader.GetInt32(reader.GetOrdinal("Crowns")),
+                            TeamMate = reader.GetString(reader.GetOrdinal("TeamMate")),
+                            Skin = reader.GetString(reader.GetOrdinal("Skin")),
+                            Count = reader.GetInt32(reader.GetOrdinal("Count")),
+                            MetK = reader.GetString(reader.GetOrdinal("MetK")),
+                            MetD = reader.GetString(reader.GetOrdinal("MetD")),
+                            Season = reader.GetString(reader.GetOrdinal("Season")),
+                            FileName = reader.GetString(reader.GetOrdinal("FileName"))
                         });
                     }
                 }
